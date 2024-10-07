c363d184b2d87f1eb5457029653917715ac432b6
Aapush01 <mdshahidafridia31@gmail.com>
2024-10-07 12:28:43 +0530

    img add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import LinkForm from "./components/LinkForm";
+import logo_LinkTrim from "../images/logo_LinkTrim.webp"
 
 function App() {
   return (
     <div className="h-screen bg-black px-3 md:px-20 pt-6">
       {/* <h1 className="text-5xl font-bold  text-[#ac6efe]">Hello world!</h1> */}
-      <img src="images/logo_LinkTrim.webp" alt="" className="h-16" />
+      <img src={logo_LinkTrim} alt="" className="h-16" />
 
       <div className="flex flex-col justify-center">
         <LinkForm />
